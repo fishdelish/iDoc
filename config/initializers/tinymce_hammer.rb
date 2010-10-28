a9a2900617b3ec6f28c0bbfcca60f4089e617fac
David Workman <workmad3@gmail.com>
2010-10-28 14:31:13 +0100

    Updated to customise more for fishdelish

--- config/initializers/tinymce_hammer.rb
+++ config/initializers/tinymce_hammer.rb
@@ -11,6 +11,6 @@ Tinymce::Hammer.init = [
   [:theme_advanced_toolbar_align, 'left'],
   [:theme_advanced_blockformats, 'h2,h3,p'],
   [:theme_advanced_toolbar_location, 'top'],
-  [:theme_advanced_buttons1, 'undo,redo,cut,copy,paste,pastetext,|,link,unlink,image,media,|,cleanup,code'],
-  [:theme_advanced_buttons2, 'formatselect,bold,italic,strikethrough,blockquote,charmap,bullist,numlist,removeformat']
+  [:theme_advanced_buttons1, 'undo,redo,cut,copy,paste,pastetext,|,link,unlink,image,media,|,cleanup,code,mysparql'],
+  [:theme_advanced_buttons2, 'formatselect,bold,italic,strikethrough,blockquote,charmap,bullist,numlist,removeformat'],
 ]
--- db/migrate/20100225151033_migrate_documentation_pages_to_tree.rb
+++ db/migrate/20100225151033_migrate_documentation_pages_to_tree.rb
@@ -3,7 +3,7 @@ class MigrateDocumentationPagesToTree < ActiveRecord::Migration
     add_column :documentation_pages, :parent_id, :integer
     add_column :documentation_pages, :position, :integer
 
-    execute("ALTER TABLE documentation_pages ADD UNIQUE(parent_id, position)")
+    execute("ALTER TABLE documentation_pages ADD CONSTRAINT documentation_pages_parent_id_key UNIQUE(parent_id, position)")
   end
 
   def self.down
